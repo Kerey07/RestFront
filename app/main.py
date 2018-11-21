cbdf27094ae2beabcafb6eed4cb864690dfcdafd
mkravchuk <kerey07@gmail.com>
2018-11-21 16:32:05 +0300

    Добавил операции

--- app/main.py
+++ app/main.py
@@ -2,8 +2,6 @@ import requests as rq
 import pandas as pd
 from app import config
 import sys
-import json
-
 
 URL = config.URL
 
@@ -14,7 +12,7 @@ def authorisation():
     while flag is False:
         login = input("Введите логин:")
         password = input("Введите пароль:")
-        payload = {'login':login, 'password':password}
+        payload = {'login': login, 'password': password}
         post = rq.post(URL + '/login', json=payload)
         message = post.json()
         print(message["message"])
@@ -24,6 +22,16 @@ def authorisation():
         else:
             print("Попробуйте еще раз.")
     return True
+
+
+def ender():
+    action = input("Выберите действие:")
+    while action not in ["x", "b"]:
+        action = input('Попробуйте еще раз:')
+    if action == "x":
+        sys.exit()
+    elif action == "b":
+        menu()
 
 
 def register():
@@ -52,30 +60,64 @@ def my_accounts():
     get = rq.get(URL + "/accounts", cookies=cookie)
     code = get.status_code
     if code == 200:
-        print("\n"*50, "Список ваших счетов")
+        print("\n" * 50, "Список ваших счетов")
         payload = get.json()
         print(table(payload))
         print("Введите b, чтобы вернуться в Главное меню\nВведите x для выхода")
-        action = input("Выберите действие:")
-        while action not in ["x", "b"]:
-            action = input('Попробуйте еще раз:')
-        if action == "x":
-            sys.exit()
-        elif action == "b":
-            menu()
+        ender()
+
 
 def history():
     get = rq.get(URL + "/history", cookies=cookie)
     payload = get.json()
     print(table(payload))
     print("Введите b, чтобы вернуться в Главное меню\nВведите x для выхода")
+    ender()
+
+
+def operation_rq(**payload):
+    print(payload)
+    post = rq.post(URL+'/operations', cookies=cookie, json=payload)
+    message = post.json()
+    print(message["message"])
+    print("Введите b, чтобы вернуться в Главное меню\nВведите x для выхода")
+    ender()
+
+
+
+def operations():
+    print("\n" * 50)
+    print(
+        "Выберите тип операции:\n1. Внесение средств.\n2. Снятие средств.\n3. Перевод средств.\n4. Создание нового счета")
+    print("\nВведите b, чтобы вернуться в Главное меню.\nВведите x для выхода.")
     action = input("Выберите действие:")
-    while action not in ["x", "b"]:
+    while action not in ["x", "b", "1", "2", "3", "4"]:
         action = input('Попробуйте еще раз:')
     if action == "x":
         sys.exit()
     elif action == "b":
         menu()
+    elif action == "1":
+        operation_type = "PURCHASE"
+        user_account = input("Введите номер своего счета:")
+        value = input("Введите сумму:")
+        operation_rq(user_account=user_account, operation_type=operation_type, value=value)
+    elif action == "2":
+        user_account = input("Введите номер своего счета:")
+        operation_type = "WITHDRAWAL"
+        value = input("Введите сумму:")
+        operation_rq(user_account=user_account, operation_type=operation_type, value=value)
+    elif action == "3":
+        operation_type = "TRANSFER"
+        user_account = input("Введите номер своего счета:")
+        recipient = input("Введите счет получателя:")
+        value = input("Введите сумму:")
+        operation_rq(user_account=user_account, recipient=recipient, operation_type=operation_type, value=value)
+    elif action == "4":
+        operation_type = "CREATE"
+        value = input("Введите сумму:")
+        operation_rq(operation_type=operation_type, value=value)
+
 
 def table(payload):
     header = []
@@ -84,13 +126,12 @@ def table(payload):
         for key in dictionary:
             if key not in header:
                 header.append(key)
-    print(header)
     table = pd.DataFrame(payload)
     return table
 
 
 def menu():
-    print("\n"*50)
+    print("\n" * 50)
     print("Добро пожаловать в Главное меню.\nВыберите действие:")
     print("1. Мои счета\n2. Операции\n3. История\n4. Выход")
     action = input('Выберите действие:')
